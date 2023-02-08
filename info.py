27fdf7bc42eb1279412809c1941180bad5f3deee
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-02-08 09:17:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -29,8 +29,8 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'heroflix_files')
+DATABASE_NAME = environ.get('DATABASE_NAME', "Sydney")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Sydney')
 
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001652564383'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '5884491121')
