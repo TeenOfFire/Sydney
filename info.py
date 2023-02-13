d336739545d7534e2288b956ea3816a1f608404a
Teen Fire <102863642+TeenOfFire@users.noreply.github.com>
2023-02-13 19:46:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,7 +60,7 @@ LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST
 LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
 
 G_FILTER = bool(environ.get("G_FILTER", True))
-SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 1200))
+SELF_DELETE_SECONDS = int(environ.get('SELF_DELETE_SECONDS', 600))
 SELF_DELETE = environ.get('SELF_DELETE', True)
 if SELF_DELETE == "True":
     SELF_DELETE = True
